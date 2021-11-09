05dca5735131b77a2574b33d4368de410fbcd848
HienDX124 <xuanhien2000678354@gmail.com>
2021-11-10 00:01:08 +0700

    Lesson 16 done

---
+++ Assets/Scripts/LightRealTime.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightRealTime : MonoBehaviour
+{
+    [SerializeField] private float DayTimeSec;
+    [SerializeField] private float CurrentTimeSec;
+
+    void Start()
+    {
+        DayTimeSec = 86400;
+        var ClockNow = System.DateTime.Now;
+        CurrentTimeSec = ClockNow.Hour * 3600 + ClockNow.Minute * 60 + ClockNow.Second;
+        Debug.Log(CurrentTimeSec);
+        var rotationVector = transform.rotation.eulerAngles;
+        rotationVector.x = 0;
+    }
+
+    void Update()
+    {
+        var rotationVector = transform.rotation.eulerAngles;
+        CurrentTimeSec += Time.deltaTime;
+        rotationVector.x = (CurrentTimeSec / DayTimeSec) * 180;
+        rotationVector.y = 0;
+        rotationVector.z = 0;
+        transform.rotation = Quaternion.Euler(rotationVector);
+    }
+}
